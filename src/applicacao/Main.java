8def3e974ecd329e2e0388ef679c2e8da1248491
LuisFenandoV14 <venturaluisf7@outlook.com>
2025-09-18 17:45:40 -0300

    Criando base do projeto

---
+++ src/applicacao/Main.java
@@ -0,0 +1,9 @@
+package applicacao;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Aqui vai ficar a inicialização dos clientes e dos controladores, assim como a chamada para cada controlador.
+
+    }
+}
