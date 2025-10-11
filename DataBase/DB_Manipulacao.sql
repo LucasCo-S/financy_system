cd74a4239dff7b0406b4dd8acf225a35d2ed9b32
Lucas C. S. <lucasscosilva@gmail.com>
2025-10-11 07:09:58 -0300

    Adicionando banco de dados e populando o mesmo

---
+++ DataBase/DB_Manipulacao.sql
@@ -0,0 +1,44 @@
+USE sistema_bancario;
+
+INSERT INTO cliente (nome, CPF, data_Nascimento) VALUES
+('João Pereira', '12345678901', '1985-06-12'),
+('Maria Silva', '98765432100', '1992-09-25'),
+('Carlos Souza', '45678912300', '1978-01-30');
+
+INSERT INTO conta (numero_Conta, saldo, id_Cliente, tipo_conta) VALUES
+('0001-01', 3500.50, 1, 'Corrente'),
+('0002-01', 7800.00, 2, 'Poupança'),
+('0003-01', 15000.00, 3, 'Investimento');
+
+INSERT INTO conta_corrente (id_conta, tarifaMensal) VALUES
+(1, 25.00);
+
+INSERT INTO conta (numero_Conta, saldo, id_Cliente, tipo_conta) VALUES
+('0006-01', 5000.00, 3, 'Poupança'),
+('0007-01', 8500.00, 1, 'Poupança');
+
+INSERT INTO conta_poupanca (id_conta, rendimento) VALUES
+(2, 0.45);
+
+INSERT INTO conta (numero_Conta, saldo, id_Cliente, tipo_conta) VALUES
+('0008-01', 30000.00, 2, 'Investimento'),
+('0009-01', 12000.00, 1, 'Investimento');
+
+INSERT INTO conta_investimento (id_conta, tipoInvestimento, valorAplicado) VALUES
+(3, 'Renda Fixa', 10000.00);
+
+INSERT INTO transacao (formaPagamento, dataPagamento, valor, id_contaOrg, id_contaDest) VALUES
+('PIX', '2025-10-11 09:15:00', 250.00, 1, 2),
+('Boleto', '2025-10-10 15:40:00', 480.00, 3, 5);
+
+INSERT INTO tran_pix (chaveOrg, chaveDest) VALUES
+('joao@banco.com', 'maria@banco.com'),
+('investidor@banco.com', 'carlos@banco.com'),
+('financeiro@banco.com', 'cliente@banco.com');
+
+INSERT INTO tran_boleto (codBarras, dataVencimento) VALUES
+('23793812938120381203120381203120381203812038', '2025-10-20'),
+('10493812938120381203120381203120381203812038', '2025-11-10'),
+('34193812938120381203120381203120381203812038', '2025-12-01');
+
+
---
+++ DataBase/DB_Schema.sql
@@ -0,0 +1,82 @@
+CREATE DATABASE IF NOT EXISTS sistema_bancario;
+
+USE sistema_bancario;
+
+CREATE TABLE cliente (
+	id_Cliente INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    CPF VARCHAR(11) NOT NULL UNIQUE,
+    data_Nascimento DATE NOT NULL
+);
+
+CREATE TABLE conta (
+	id_Conta INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    numero_Conta VARCHAR(15) NOT NULL,
+    saldo DECIMAL(15,2) DEFAULT 0.0,
+    data_Abertura TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_Cliente INT UNSIGNED NOT NULL,
+    tipo_conta VARCHAR(25) NOT NULL,
+    
+    CONSTRAINT fk_ClienteConta FOREIGN KEY (id_Cliente) REFERENCES Cliente (id_Cliente)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+
+CREATE TABLE conta_corrente(
+	id_conta INT UNSIGNED PRIMARY KEY,
+    tarifaMensal DECIMAL(5,2) NOT NULL, 
+    
+    CONSTRAINT FK_contaCOR FOREIGN KEY (id_conta) REFERENCES conta(id_conta)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE conta_poupanca(
+	id_conta INT UNSIGNED PRIMARY KEY,
+    dataRendimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	rendimento DECIMAL(5,2) NOT NULL,
+    
+    CONSTRAINT FK_contaPOU FOREIGN KEY (id_conta) REFERENCES conta(id_conta)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE conta_investimento(
+	id_conta INT UNSIGNED PRIMARY KEY,
+    tipoInvestimento VARCHAR(63) NOT NULL,
+    valorAplicado DECIMAL(11, 2) NOT NULL,
+    
+    CONSTRAINT FK_contaINV FOREIGN KEY (id_conta) REFERENCES conta(id_conta)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE transacao(
+	id_transacao INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    formaPagamento VARCHAR(127) NOT NULL,
+    dataPagamento TIMESTAMP NOT NULL,
+    valor DECIMAL(13,2) NOT NULL,
+    
+    id_contaOrg INT UNSIGNED,
+    id_contaDest INT UNSIGNED,
+    
+    CONSTRAINT FK_pagContaOrg FOREIGN KEY (id_contaOrg) REFERENCES conta(id_conta)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT FK_pagContaDest FOREIGN KEY (id_contaDest) REFERENCES conta(id_conta)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE tran_pix(
+	id_transacao INT UNSIGNED PRIMARY KEY,
+	chaveOrg VARCHAR(255) NOT NULL,
+    chaveDest VARCHAR(255) NOT NULL,
+    
+    CONSTRAINT FK_pagPIX FOREIGN KEY (id_transacao) REFERENCES transacao(id_transacao)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE tran_boleto(
+	id_transacao INT UNSIGNED PRIMARY KEY,
+	codBarras VARCHAR(127) NOT NULL,
+    dataVencimento DATE NOT NULL,
+    
+    CONSTRAINT FK_pagBOL FOREIGN KEY (id_transacao) REFERENCES transacao(id_transacao)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
